7d1bfd5b2bebe367d0566bf092aa189d6db64b0c
Amp-Apirak <apirak.ba@gmail.com>
2024-08-28 10:24:56 +0700

    Insert code demo

---
+++ app/pages/user/user.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { NavItem } from '@nuxt/content'
+
+const navigation = inject<Ref<NavItem[]>>('navigation', ref([]))
+</script>
+
+<template>
+  <UContainer>
+    <UPage>
+      <template #left>
+        <UAside>
+          <UNavigationTree :links="mapContentNavigation(navigation)" />
+        </UAside>
+      </template>
+
+      <!-- Change this to `<slot />` when in a layout. -->
+      <NuxtPage />
+    </UPage>
+  </UContainer>
+</template>
