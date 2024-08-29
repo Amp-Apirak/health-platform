59d24616740cea6a776ccfa512f607767b833d57
Amp-Apirak <apirak.ba@gmail.com>
2024-08-29 11:41:52 +0700

    A

--- app/pages/organization.vue
+++ app/pages/organization.vue
@@ -282,23 +282,7 @@ const paginatedFilteredRows = computed(() => {
   <UDashboardPage>
     <UDashboardPanel grow>
       <!-- เริ่มแถบด้านบน -->
-      <UDashboardNavbar title="Organization Management">
-        <template #right>
-          <!-- ปุ่มการแจ้งเตือน -->
-          <UTooltip text="Notifications" :shortcuts="['N']">
-            <UButton
-              color="gray"
-              variant="ghost"
-              square
-              @click="isNotificationsSlideoverOpen = true"
-            >
-              <UChip color="red" inset>
-                <UIcon name="i-heroicons-bell" class="w-5 h-5" />
-              </UChip>
-            </UButton>
-          </UTooltip>
-        </template>
-      </UDashboardNavbar>
+      <AppAppber />
       <!-- จบแถบด้านบน -->
 
       <UDashboardPanelContent>
